dcc413ae4da8bd3fc8f50338690201e45092e8b1
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-08-03 07:21:39 +0300

    updated

--- Dockerizer/main.py
+++ Dockerizer/main.py
@@ -1,11 +1,18 @@
 import git
 import os
 
+
+def find_file(filename, path, image):
+   for root, dir, files in os.walk(path):
+      if filename in files:
+         return os.system(f"docker build . -t {image} && docker push {image}")
+   return False
+
 while True:
-    url = input("Please paste repo url. ")
-    inputted_branch = input("Please specify Branch. ")
+    repo = os.environ['REPO']
+    image_name = os.environ['IMAGE']
     try:
-        repo = git.Repo.clone_from(url, os.path.join("./", 'repo'), branch=inputted_branch)
+        repo = git.Repo.clone_from(repo, os.path.join("./", 'repo'), branch="main")
         with open('./repo/Dockerfile', 'w') as fp:
             pass
             # To write data to new file uncomment
