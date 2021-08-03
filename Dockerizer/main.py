b427c41054ff35a1d10236d7931be18d2a4b87f1
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-08-03 07:15:28 +0300

    Project downloads repo and adds dockerfile

--- Dockerizer/main.py
+++ Dockerizer/main.py
@@ -1,2 +1,17 @@
 import git
 import os
+
+while True:
+    url = input("Please paste repo url. ")
+    inputted_branch = input("Please specify Branch. ")
+    try:
+        repo = git.Repo.clone_from(url, os.path.join("./", 'repo'), branch=inputted_branch)
+        with open('./repo/Dockerfile', 'w') as fp:
+            pass
+            # To write data to new file uncomment
+            fp.write("New file created")
+        print("Repo cloned succesfully")
+        break
+    except:
+        print ("Repo could not be downloaded")
+    
