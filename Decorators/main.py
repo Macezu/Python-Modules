6d96807415134eb9e9e7ae7df1073aea79607a7a
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-11-03 17:55:15 +0200

    the more sleek method of decorating

--- Decorators/main.py
+++ Decorators/main.py
@@ -5,15 +5,22 @@ def decorator_function(original_function):
         return original_function()
     return wrapper_function
 
-
-
 def display():
     print("display function ran")
 
-
 decorated_display = decorator_function(display)
-
 decorated_display()
-
+print("----------")
 #Or The other way
 
+def decorator_function2(original_function):
+    def wrapper_function():
+        print("i too ran before the og_function")
+        return original_function()
+    return wrapper_function
+
+@decorator_function2
+def display2():
+    print("display2 has ran, ggs")
+
+display2()
